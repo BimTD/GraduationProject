92b9443f156f536db3cf94049178cb24c5e99eb9
TranDuyBim <tranduybim2906@gmail.com>
2025-07-18 17:04:38 +0700

    update giao dien product

--- src/main/java/org/example/graduationproject/controllers/admin/ProductController.java
+++ src/main/java/org/example/graduationproject/controllers/admin/ProductController.java
@@ -1,5 +1,6 @@
 package org.example.graduationproject.controllers.admin;
 
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,8 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 @PreAuthorize("hasRole('ADMIN')")
-public class AdminController {
+public class ProductController {
+    @GetMapping("/product")
+    public String productPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
 
+        model.addAttribute("username", username);
+        model.addAttribute("currentPage", "product");
 
+        return "admin/product";
+    }
+
+    @GetMapping("/productForm")
+    public String productForm() {
+
+        return "admin/form-product";
+    }
 }
-
