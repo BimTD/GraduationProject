bd74054b9601af1b1f5e686b206ba023c4236da8
TranDuyBim <tranduybim2906@gmail.com>
2025-07-20 19:58:20 +0700

    update

--- src/main/java/org/example/graduationproject/controllers/admin/ProductController.java
+++ src/main/java/org/example/graduationproject/controllers/admin/ProductController.java
@@ -18,10 +18,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -86,6 +83,13 @@ public class ProductController {
         model.addAttribute("nhaCungCaps", nhaCungCaps);
 
         return "admin/form-product";
+    }
+
+    @PostMapping("/productForm")
+    public String addProduct(@RequestParam("file") MultipartFile[] files,
+                             @ModelAttribute SanPham sanPham,
+                             RedirectAttributes redirectAttributes) throws IOException {
+        return null;
     }
 
 }
